9c66d47816a59ae3909f057ac7210e22cf2c49e4
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2024-12-05 21:43:30 -0300

    cria elemento animal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import Animal from "./Animal";
+
 
 function App(){
-  return <div className="app">Olá Mundo!</div>
+  return <div className="app">Olá Mundo!
+    <Animal />
+  </div>
 }
 
 export default App;
