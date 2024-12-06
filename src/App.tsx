24ee8a7b3c15c0734ca9daca8c1e33f3e7ac0683
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2024-12-05 22:29:22 -0300

    alterando codigo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App(){
   return (
     <div className="app">
+
       <Animal icone="🦁" nome="Leão" peso={190.0} emExtincao={true} />
       <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
       <Animal icone="🦒" nome="Girafa" peso={1200.0} emExtincao={true} />
