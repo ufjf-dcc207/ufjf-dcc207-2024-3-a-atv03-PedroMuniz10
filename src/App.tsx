1f7a0225cf0463737f5bf29b2bbad33322497bd0
PedroMuniz10 <pedro.lau@estudante.ufjf.br>
2024-12-09 20:05:41 -0300

    continuando App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import "./App.css";
 import Animal from "./Animal";
 import Exibicao from "./Exibicao";
+import {  ReactNode } from "react";
+
+type AnimaisTupleType = [string, string, number, boolean];
+
+const ANIMAIS: AnimaisTupleType[] = [
+  ["🦁", "Leão", 190.0, true],
+  ["🦩", "Flamingo", 12.0, true],
+  ["🦒", "Girafa", 1200.0, true],
+  ["🦜", "Papagaio", 0.4, false],
+];
 
 function App(){
+  const exA1: ReactNode[] = [];
+  const exB2: ReactNode[] = ANIMAIS.map((animal) => (
+    <Animal
+      key={animal[1]}
+      icone={animal[0]}
+      nome={animal[1]}
+      peso={animal[2]}
+      emExtincao={animal[3]}
+    />
+  ));
+
   return (
     <div className="app">
       <Exibicao 
         inicio={new Date("2024-12-02T08:00:00.000-03:00")}
         fim={new Date("2024-12-02T12:00-03:00")}
         local="A1"
-      >
-          <Animal icone="🦁" nome="Leao" peso={190.0} emExtincao={true} />
-          <Animal icone="🦩" nome="Flamingo" peso={12.0} emExtincao={true} />
-      </Exibicao>
+      >{exA1}</Exibicao>
 
-      <Exibicao 
+      <Exibicao
         inicio={new Date("2024-12-02T14:00-03:00")}
         fim={new Date("2024-12-02T16:00-03:00")}
         local="B2"
-      >
-          <Animal icone="🦒" nome="Girafa" peso={1200.0} emExtincao={true} />
-          <Animal icone="🦜" nome="Papagaio" peso={0.4} emExtincao={false} />
-      </Exibicao>
+      >{exB2}</Exibicao>
     </div>
   );
 }
